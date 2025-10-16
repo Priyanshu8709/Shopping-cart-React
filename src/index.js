7404e883b3edbc249d15462b08dd87c3e5dbbb92
Priyanshu Raj <priyanshurajsk_cse23@its.edu.in>
2025-10-16 21:29:25 +0530

    fixed issues

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Toaster} from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/Shopping-cart-React">
+  <HashRouter>
     <Provider store={store}>
       <App />
       <Toaster/>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
   
 );
